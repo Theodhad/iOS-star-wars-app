8c98a434e3a15e77d671b6411deff0d495f67efd
Jonathan Strempel <Theodhad@Jonathans-MacBook-Pro.local>
2016-06-22 13:15:19 -0700

    added files

--- Proj1/ViewController.swift
+++ Proj1/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var BG: UIImageView!
+    @IBOutlet weak var Side: UIImageView!
+    @IBOutlet weak var Republic: UIImageView!
+    @IBOutlet weak var Sith: UIImageView!
+    @IBOutlet weak var Repub_button: UIButton!
+    @IBOutlet weak var Sith_button: UIButton!
+    @IBOutlet weak var reset: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func Repub(sender: AnyObject) {
+        Sith.hidden = true
+        Sith_button.hidden = true
+        Republic.hidden = false
+        Repub_button.hidden = false
+        Side.hidden = true
+        reset.hidden = false
+        
+    }
+    @IBAction func Sith(sender: AnyObject) {
+        Sith.hidden = false
+        Sith_button.hidden = false
+        Republic.hidden = true
+        Repub_button.hidden = true
+        Side.hidden = true
+        reset.hidden = false
+    }
+    @IBAction func Reset(sender: AnyObject) {
+        Sith.hidden = false
+        Sith_button.hidden = false
+        Republic.hidden = false
+        Repub_button.hidden = false
+        Side.hidden = false
+        reset.hidden = true
+    }
 }
 
